261158dcbf09ead15da902e3d3e24732aaf02e6e
LeeJeongWook <leejw0709@gmail.com>
2021-05-19 23:27:18 +0900

    Update [7523]Gauß_.cpp

--- Online_Judge/BaekJoon/C++배우기(251-300)/[7523]Gauß_.cpp
+++ Online_Judge/BaekJoon/C++배우기(251-300)/[7523]Gauß_.cpp
@@ -20,24 +20,32 @@ int main() {
 		ans = 0;
 		cin >> n >> m;
 		cnt = abs(n + m);
-
-		if ( (n + m) < 0 ) {
+		long long a = abs(n + m - 1);
+		//ans = (n + m)* ((n + m) - 1) / 2;
+		if ((n + m) < 0) {
+			cout << " 111 " << endl;
+			ans = (n + m) * ((a / 2) + (a % 2));
+			/*
 			for (int i = n; i < n + cnt; i++) {
 				ans += i;
 			}
+			*/
 		}
 		else if ((n < 0) && (0 <= (n + m))) {
+			cout << " 222 " << endl;
+			//ans = (n + m) * (((n + m) / 2) + ((n + m) % 2));
+			ans = (n + m) * ((a / 2) + (a % 2));
+			/*
 			int i = -n;
 			while (cnt > 0) {
 				i++;
 				ans += i;
 				cnt--;
 			}
+			*/
 		}
 		else {
-			for (int i = n; i < cnt; i++) {
-				ans += i;
-			}
+			ans = (n + m) * ((a / 2) + (a % 2));
 		}
 		cout << "Scenario #" << testcase << ":\n" << ans << "\n\n";
 	}
