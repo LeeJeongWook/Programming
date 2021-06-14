181863b6bc1a0edc1ab7ab7482eeb24b19b2b2a5
leejw0709@gmail.com <leejw0709@gmail.com>
2021-06-14 23:41:22 +0900

    Update [1431]시리얼 번호.cpp

--- Online_Judge/BaekJoon/C++배우기(301-350)/[1431]시리얼 번호.cpp
+++ Online_Judge/BaekJoon/C++배우기(301-350)/[1431]시리얼 번호.cpp
@@ -10,7 +10,6 @@ struct serial {
 	int size;
 	int sum;
 };
-typedef
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -19,39 +18,14 @@ int main() {
 
 	freopen("input.txt", "r", stdin);
 
-	int N, cnt, tmp11, tmp22;
+	int N;
 	string str;
-	char tmp1, tmp2;
 	vector<string> v;
 
-	cin >> N;
-
-	for (int test = 0; test < N; test++) {
+	for (int test = 1; test <= N; test++) {
 		cin >> str;
-		cnt = 0;
-
-		str.size();
-
-		for (int i = 0; i < str.size(); i++) {
-			if (str[i] <= 57) {
-				cnt += str[i] - '0';
-			}
-		}
 		v.push_back(str);
 	}
 
-	sort(v.begin(), v.end());
-	
-	for (string s : v) {
-		cout << s << '\n';
-	}
-
-	tmp1 = '0';
-	tmp2 = '9';
-
-	tmp11 = tmp1;
-	tmp22 = tmp2;
-	cout << (int)tmp11 << "   " << (int)tmp22 << endl;
-	
 	return 0;
 }
