ec597a7a09bbbfd1297e6a3658da7be1158ab170
leejw0709@gmail.com <leejw0709@gmail.com>
2021-06-25 23:51:50 +0900

    Create [2703]Cryptoquote.cpp

---
+++ Online_Judge/BaekJoon/C++배우기(301-350)/[2703]Cryptoquote.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+
+
+	return 0;
+}
