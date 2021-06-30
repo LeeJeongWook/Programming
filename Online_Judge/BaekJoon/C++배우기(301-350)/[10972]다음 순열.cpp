1e6202de93b28ba21adb5249774bebaf4597c963
leejw0709@gmail.com <leejw0709@gmail.com>
2021-06-30 23:52:42 +0900

    Update [10972]다음 순열.cpp

--- Online_Judge/BaekJoon/C++배우기(301-350)/[10972]다음 순열.cpp
+++ Online_Judge/BaekJoon/C++배우기(301-350)/[10972]다음 순열.cpp
@@ -7,25 +7,44 @@
 
 using namespace std;
 
-bool check[MAX] = { 0, };
+int arr[MAX];
+bool check[MAX];
+bool flag = false;
 vector<int> v;
+vector<int> a;
 
-void Permutation(int depth, int r) {
-	if (depth == r) {
-		for (int i = 0; i < v.size(); i++) {
-			cout << v[i] << " ";
+bool Compare(vector<int> a, vector<int> b) {
+	bool result = true;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != b[i]) {
+			result = false;
+			break;
 		}
-		cout << '\n';
+	}
+	return result;
+}
+
+void Permutation(int n, int r) {
+	if (n == r) {
+		if (flag) {
+			for (int i = 0; i < v.size(); i++)
+				cout << v[i] << " ";
+			flag = false;
+			return;
+		}
+		if (Compare(a, v)) {
+			flag = true;
+		}
 		return;
 	}
 
-	for (int i = 1; i < r; i++) {
-		if (!check[i]) {
-			check[i] = true;
-			v[depth] = i;
-			Permutation(depth + 1, r);
-			check[i] = false;
-		}
+	for (int i = 0; i < r; i++) {
+		if (check[i] == true) continue;
+		check[i] = true;
+		v.push_back(arr[i]);
+		Permutation(n + 1, r);
+		v.pop_back();
+		check[i] = false;
 	}
 }
 
@@ -36,12 +55,19 @@ int main() {
 
 	freopen("input.txt", "r", stdin);
 
-	int N;
-	
+	int N, tmp;
+
 	cin >> N;
-	
-	for (int i = 1; i <= N; i++)
-		v.push_back(i);
+
+	for (int i = 1; i <= N; i++) {
+		cin >> tmp;
+		a.push_back(tmp);
+		arr[i - 1] = i;
+	}
+
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << " ";
+	cout << endl;
 
 	Permutation(0, N);
 
