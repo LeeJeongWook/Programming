7e46a8a2b13684449ee790c5ce9151764415d344
leejw0709@gmail.com <leejw0709@gmail.com>
2021-07-02 23:01:49 +0900

    Update [10972]다음 순열.cpp

--- Online_Judge/BaekJoon/C++배우기(301-350)/[10972]다음 순열.cpp
+++ Online_Judge/BaekJoon/C++배우기(301-350)/[10972]다음 순열.cpp
@@ -1,53 +1,10 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
-#include <filesystem>
 #include <vector>
-
-#define MAX 10001
+#include <algorithm>
 
 using namespace std;
 
-int arr[MAX];
-bool check[MAX];
-bool flag = false;
-vector<int> v;
-vector<int> a;
-vector<int> b;
-
-bool Compare(vector<int> a, vector<int> b) {
-	bool result = true;
-	for (int i = 0; i < a.size(); i++) {
-		if (a[i] != b[i]) {
-			result = false;
-			break;
-		}
-	}
-	return result;
-}
-
-void Permutation(int n, int r) {
-	if (n == r) {
-		if (flag) {
-			for (int i = 0; i < r; i++)
-				b.push_back(v[i]);
-			flag = false;
-			return;
-		}
-		if (Compare(a, v)) {
-			flag = true;
-		}
-		return;
-	}
-
-	for (int i = 0; i < r; i++) {
-		if (check[i] == true) continue;
-		check[i] = true;
-		v.push_back(arr[i]);
-		Permutation(n + 1, r);
-		v.pop_back();
-		check[i] = false;
-	}
-}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -59,21 +16,18 @@ int main() {
 	int N, tmp;
 
 	cin >> N;
+	
+	vector<int> v(N);
 
-	for (int i = 1; i <= N; i++) {
-		cin >> tmp;
-		a.push_back(tmp);
-		arr[i - 1] = i;
-	}
+	for (int i = 1; i <= N; i++)
+		v[i - 1] = i;
 
-	Permutation(0, N);
+	do {
+		for (int i = 0; i < N; i++)
+			cout << v[i] << " ";
 
-	if (b.size() < N)
-		cout << -1;
-	else {
-		for (int i = 0; i < b.size(); i++)
-			cout << b[i] << " ";
-	}
+		cout << '\n';
+	}while(prev_permutation(v.begin(), v.end())
 
 	return 0;
 }
