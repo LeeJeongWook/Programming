a8c1e8f8f6be87474878de9a50b5dbf47f11a880
leejw0709@gmail.com <leejw0709@gmail.com>
2021-07-05 23:27:54 +0900

    Create [10973]이전 순열.cpp

---
+++ Online_Judge/BaekJoon/C++배우기(301-350)/[10973]이전 순열.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int N;
+
+	cin >> N;
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+	if (prev_permutation(v.begin(), v.end())) {
+		for (int i = 0; i < N; i++)
+			cout << v[i] << ' ';
+	}
+	else cout << -1;
+
+	return 0;
+}
