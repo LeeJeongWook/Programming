ceaa5547ed1618b0623d3203e9995869270daab7
leejw0709@gmail.com <leejw0709@gmail.com>
2021-07-15 22:41:14 +0900

    Create [9655]돌 게임.cpp

---
+++ Online_Judge/BaekJoon/C++배우기(351-400)/[9655]돌 게임.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int N;
+
+	cin >> N;
+
+	(N % 2) == 0 ? cout << "CY" : cout << "SK";
+
+	return 0;
+}
