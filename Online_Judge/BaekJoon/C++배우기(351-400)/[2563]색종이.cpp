f3e78cda44f38d708d5093ae470061ef91d0b805
leejw0709@gmail.com <leejw0709@gmail.com>
2021-07-23 23:54:13 +0900

    Create [2563]색종이.cpp

---
+++ Online_Judge/BaekJoon/C++배우기(351-400)/[2563]색종이.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	int T, a, b;
+
+
+	return 0;
+}
