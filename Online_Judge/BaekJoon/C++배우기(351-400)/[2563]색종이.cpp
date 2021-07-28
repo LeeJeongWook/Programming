4f19cb674a1b213ed76a104e772863cc1e647372
leejw0709@gmail.com <leejw0709@gmail.com>
2021-07-28 23:47:56 +0900

    Update [2563]색종이.cpp

--- Online_Judge/BaekJoon/C++배우기(351-400)/[2563]색종이.cpp
+++ Online_Judge/BaekJoon/C++배우기(351-400)/[2563]색종이.cpp
@@ -10,8 +10,29 @@ int main() {
 
 	freopen("input.txt", "r", stdin);
 
-	int T, a, b;
+	int pcnt, x, y, paper[101][101] = { 0, }, ans = 0;
 
+	cin >> pcnt;
+
+	for (int test = 0; test < pcnt; test++) {
+		cin >> x >> y;
+
+		for (int i = x; i < x + 10; i++) {
+			for (int j = y; j < y + 10; j++) {
+				paper[i][j]++;
+			}
+		}
+	}
+
+	for (int i = 0; i < 101; i++) {
+		for (int j = 0; j < 101; j++) {
+			if (paper[i][j] != 0) {
+				ans++;
+			}
+		}
+	}
+
+	cout << ans;
 
 	return 0;
 }
