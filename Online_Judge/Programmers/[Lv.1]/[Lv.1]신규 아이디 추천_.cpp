ba5491c9b81fe820c9c831cbf8deedb6eaf3ef45
leejw0709@gmail.com <leejw0709@gmail.com>
2021-08-19 23:51:09 +0900

    -

--- Online_Judge/Programmers/[Lv.1]/[Lv.1]신규 아이디 추천_.cpp
+++ Online_Judge/Programmers/[Lv.1]/[Lv.1]신규 아이디 추천_.cpp
@@ -1,18 +1,16 @@
-#include <iostream>
-
 #include <string>
 #include <vector>
 
 using namespace std;
 
 string solution(string new_id) {
-    string answer = "";
 
-    for (int i = 0; i < new_id.size(); i++) {
-        //1단계
+    //1단계
+    for (int i = 0; i < new_id.size(); i++)
         new_id[i] = tolower(new_id[i]);
 
-        //2단계
+    //2단계
+    for (int i = 0; i < new_id.size(); i++) {
         if (!((97 <= (int)new_id[i]) && ((int)new_id[i] <= 122)) &&
             !((48 <= (int)new_id[i]) && ((int)new_id[i] <= 57)) &&
             (new_id[i] != '-') && (new_id[i] != '_') && (new_id[i] != '.')) {
@@ -20,8 +18,10 @@ string solution(string new_id) {
             i--;
             continue;
         }
+    }
 
-        //3단계
+    //3단계
+    for (int i = 0; i < new_id.size(); i++) {
         if ((new_id[i] == '.') && (new_id[i + 1] == '.')) {
             new_id.erase(new_id.begin() + i);
             i--;
@@ -29,37 +29,19 @@ string solution(string new_id) {
         }
     }
 
-
     //4단계
-    while ((new_id[0] == '.') || (new_id[new_id.size() - 1] == '.')) {
-        if (new_id[0] == '.') {
-            new_id.erase(new_id.begin());
-        }
-
-        if (new_id[new_id.size() - 1] == '.') {
-            new_id.erase(new_id.begin() + new_id.size() - 1);
-        }
-    }
+    if (new_id.front() == '.') new_id.erase(new_id.begin());
+    if (new_id.back() == '.') new_id.erase(new_id.end() - 1);
 
     //5단계
-    if (new_id.empty())
-        new_id.push_back('a');
+    if (new_id.empty()) new_id.push_back('a');
 
     //6단계
-    if (16 <= new_id.size()) {
-        new_id.erase(new_id.begin() + 15, new_id.end());
-    }
-
-    if (new_id[new_id.size() - 1] == '.') {
-        new_id.erase(new_id.begin() + new_id.size() - 1);
-    }
+    if (16 <= new_id.size()) new_id.erase(new_id.begin() + 15, new_id.end());
+    if (new_id.back() == '.') new_id.erase(new_id.end() - 1);
 
     //7단계
-    while (new_id.size() <= 2) {
-        new_id.push_back(new_id[new_id.size() - 1]);
-    }
+    while (new_id.size() <= 2) new_id.push_back(new_id.back());
 
-    answer = new_id;
-
-    return answer;
+    return new_id;
 }
