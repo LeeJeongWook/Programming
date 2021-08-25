a385f83fdc98de926558fe123a8ab874bc9831c4
leejw0709@gmail.com <leejw0709@gmail.com>
2021-08-25 21:43:11 +0900

    Update [Lv.1]수박수박수박수박수박수.cpp

--- Online_Judge/Programmers/[Lv.1]/[Lv.1]수박수박수박수박수박수.cpp
+++ Online_Judge/Programmers/[Lv.1]/[Lv.1]수박수박수박수박수박수.cpp
@@ -7,9 +7,9 @@ string solution(int n) {
     string answer;
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0)
-            answer.push_back("박");
+            answer += "수";
         else
-            answer.push_back("수");
+            answer += "박";
     }
     return answer;
 }
