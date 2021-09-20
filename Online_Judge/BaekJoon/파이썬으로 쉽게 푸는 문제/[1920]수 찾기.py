dbb6fb1fe615049d635c97170296c3ec6cf2e714
leejw0709@gmail.com <leejw0709@gmail.com>
2021-09-20 22:40:19 +0900

    -

---
+++ Online_Judge/BaekJoon/파이썬으로 쉽게 푸는 문제/[1920]수 찾기.py
@@ -0,0 +1,20 @@
+n = input()
+N = sorted(map(int, input().split()))
+m = input()
+M = sorted(map(int, input().split()))
+
+def binary(l, N, start, end):
+    if start > end:
+        return 0
+    m = (start+end)//2
+    if l == N[m]:
+        return 1
+    elif l < N[m]:
+        return binary(l, N, start, m-1)
+    else:
+        return binary(l, N, m+1, end)
+
+for l in M:
+    start = 0
+    end = len(N)-1
+    print(binary(l, N, start, end))
