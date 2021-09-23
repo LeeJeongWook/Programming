1db4daadd3eb49ea58473f524a73b40fafeedc06
leejw0709@gmail.com <leejw0709@gmail.com>
2021-09-23 22:33:03 +0900

    Update [1252]2진수 덧셈.py

--- Online_Judge/BaekJoon/파이썬으로 쉽게 푸는 문제/[1252]2진수 덧셈.py
+++ Online_Judge/BaekJoon/파이썬으로 쉽게 푸는 문제/[1252]2진수 덧셈.py
@@ -4,23 +4,27 @@ idx = len(b) - 1
 print(type(idx))
 print(idx)
 
-ans = []
 advance = 0
 tmp = 0
 
+ans = []
+
 for i in reversed(a):
     i = int(i)
-    b[idx] = int(b[idx])
 
+    print(idx, advance)
+#     print("idx:", idx, " advence:", advence, "b[idx]:", b[idx], end == "")
     if idx > -1:
         if advance == 1:
-            tmp = i + b[idx] + 1
+            tmp = i + int(b[idx]) + 1
+            advance = 0
         else:
-            tmp = i + b[idx]
+            tmp = i + int(b[idx])
 
     if tmp > 1:
         advance = 1
         tmp -= 2
+
     print("tmp:", tmp)
     ans.insert(0, tmp)
     idx -= 1
