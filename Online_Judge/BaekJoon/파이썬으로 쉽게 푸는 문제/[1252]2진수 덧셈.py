dcac0d5d414f104d82b79893a920d0c02d032c2f
leejw0709@gmail.com <leejw0709@gmail.com>
2021-09-24 23:51:53 +0900

    Update [1252]2진수 덧셈.py

--- Online_Judge/BaekJoon/파이썬으로 쉽게 푸는 문제/[1252]2진수 덧셈.py
+++ Online_Judge/BaekJoon/파이썬으로 쉽게 푸는 문제/[1252]2진수 덧셈.py
@@ -1,32 +1,6 @@
-a,b = input().split()
-print(a,b)
-idx = len(b) - 1
-print(type(idx))
-print(idx)
+A, B = map(str, input().split())
+A = int(A, 2)
+B = int(B, 2)
+C = A + B
 
-advance = 0
-tmp = 0
-
-ans = []
-
-for i in reversed(a):
-    i = int(i)
-
-    print(idx, advance)
-#     print("idx:", idx, " advence:", advence, "b[idx]:", b[idx], end == "")
-    if idx > -1:
-        if advance == 1:
-            tmp = i + int(b[idx]) + 1
-            advance = 0
-        else:
-            tmp = i + int(b[idx])
-
-    if tmp > 1:
-        advance = 1
-        tmp -= 2
-
-    print("tmp:", tmp)
-    ans.insert(0, tmp)
-    idx -= 1
-
-print(ans)
+print(bin(C)[2:])
