51cf125aa7a62680a9c0a05f179c2d8dbdab9650
leejw0709@gmail.com <leejw0709@gmail.com>
2021-10-01 21:08:20 +0900

    Update [5355]화성 수학_.py

--- Online_Judge/BaekJoon/5000-9999/[5355]화성 수학_.py
+++ Online_Judge/BaekJoon/5000-9999/[5355]화성 수학_.py
@@ -1,2 +1,16 @@
-print(399)
-print("leejeongwook")
+T = int(input())
+
+for i in range(0, T):
+    L = input().split()
+    ans = 0
+
+    for j in range(len(L)):
+        if j == 0:
+            ans += float(L[j])
+        elif L[j] == '@':
+            ans *= 3
+        elif L[j] == '%':
+            ans += 5
+        elif L[j] == '#':
+            ans -= 7
+    print("%0.2f" % ans)
