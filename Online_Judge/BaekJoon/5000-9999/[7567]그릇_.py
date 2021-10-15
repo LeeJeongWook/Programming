7d781ab8fd807bcfc10390c4865ce356955bde52
leejw0709@gmail.com <leejw0709@gmail.com>
2021-10-15 23:45:17 +0900

    Update [7567]그릇_.py

--- Online_Judge/BaekJoon/5000-9999/[7567]그릇_.py
+++ Online_Judge/BaekJoon/5000-9999/[7567]그릇_.py
@@ -1,2 +1,13 @@
-print(399)
-print("leejeongwook")
+dishes = input()
+
+flag = 0
+
+height = 0
+
+for i in dishes:
+    if i == flag:
+        height += 5
+    else:
+        height += 10
+        flag = i
+print(height)
