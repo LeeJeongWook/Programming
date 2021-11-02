9b807c93421528b8d6940e01d8450005d2438f20
leejw0709@gmail.com <leejw0709@gmail.com>
2021-11-02 23:50:28 +0900

    Update [1120]문자열.py

--- Online_Judge/BaekJoon/0000-4999/[1120]문자열.py
+++ Online_Judge/BaekJoon/0000-4999/[1120]문자열.py
@@ -1,15 +1,18 @@
 A, B = input().split()
 cnt = 0
 max = 0
-if len(A) != len(B):
 
+if len(A) != len(B):
     for i in range(len(A)):
         tmp = 0
         idx = i
         for j in range(len(B)):
-            if A[idx] == B[j-1]:
+            if A[idx] == B[j]:
                 idx += 1
                 tmp += 1
+        if max < tmp:
+            max = tmp
+    cnt = len(A) - max
 else:
     for i in range(len(A)):
         if A[i-1] != B[i-1]:
