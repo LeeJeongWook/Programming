cc26917b6104495284cfab2ae3e2d7a571a02ca6
leejw0709@gmail.com <leejw0709@gmail.com>
2021-11-04 00:22:20 +0900

    Update [1059]좋은 구간.py

--- Online_Judge/BaekJoon/0000-4999/[1059]좋은 구간.py
+++ Online_Judge/BaekJoon/0000-4999/[1059]좋은 구간.py
@@ -1,9 +1,16 @@
 L = int(input())
-S = set()
-
 nums = [int(x) for x in input().split()]
+n = int(input())
 
-S.update(nums)
+nums.sort()
+ans = 0
 
-print(S)
-    
+for i in range(L - 1):
+    if nums[i] < n < nums[i+1]:
+        tmp = nums[i+1] - n
+        for j in range(n - (nums[i]) -1):
+            ans += tmp
+        ans += nums[i+1] - n - 1
+        break
+
+print(ans)
