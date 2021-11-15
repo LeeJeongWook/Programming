bacc6cea1b772be803ac1483bfdfb3c102b46a1f
leejw0709@gmail.com <leejw0709@gmail.com>
2021-11-15 22:55:03 +0900

    Update [1246]온라인 판매.py

--- Online_Judge/BaekJoon/0000-4999/[1246]온라인 판매.py
+++ Online_Judge/BaekJoon/0000-4999/[1246]온라인 판매.py
@@ -1,13 +1,13 @@
 N, M = map(int, input().split())
 
 P = [int(input()) for _ in range(M)]
-P.sort(reverse = True)
+P.sort()
+price, ans = 0, 0
 
-max = 0
-num = int(len(P))
-for i in reversed(P):
-    if max < (i * num):
-        max = i * num
-        cost = i
-    num -= 1
-print (cost, cost * (P.index(cost) + 1))
+for i in range(M):
+    mymax = min(M-i, N)
+    if ans < P[i] * mymax:
+        price = P[i]
+        ans = P[i] * mymax
+
+print(price, ans)
