e09c46c4ad27100e4107a62df0a24a24a935cdc5
LeeJeongWook <leejw0709@gmail.com>
2022-01-12 16:32:00 +0900

    Update [1672]DNA 해독.py

--- Online_Judge/BaekJoon/0000-4999/[1672]DNA 해독.py
+++ Online_Judge/BaekJoon/0000-4999/[1672]DNA 해독.py
@@ -1,5 +1,25 @@
+table = {"AA":'A', "AG":'C', "AC":'A', "AT":'G',
+         "GA":'C', "GG":'G', "GC":'T', "GT":'A',
+         "CA":'A', "CG":'T', "CC":'C', "CT":'G',
+         "TA":'G', "TG":'A', "TC":'G', "TT":'T'}
+
+N = int(input())
+DNA = list(input().rstrip())
+
+while len(DNA) > 1:
+    a = DNA.pop()
+    b = DNA.pop()
+    DNA.append(table[a+b])
+print(DNA.pop())
+
+'''
+# 시간 초과
+
 idx = ['A', 'G', 'C', 'T']
-table = [['A', 'C', 'A', 'G'], ['C', 'G', 'T', 'A'], ['A', 'T', 'C', 'G'], ['G', 'A', 'G', 'T']]
+table = [['A', 'C', 'A', 'G'],
+         ['C', 'G', 'T', 'A'],
+         ['A', 'T', 'C', 'G'],
+         ['G', 'A', 'G', 'T']]
 
 N = int(input())
 string = input()
@@ -12,3 +32,4 @@ while True:
     string += tmp
 
 print(string)
+'''
