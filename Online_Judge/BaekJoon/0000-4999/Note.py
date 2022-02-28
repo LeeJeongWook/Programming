62bf5f37f0306611f2c70eddf0d326ff2a044d84
LeeJeongWook <leejw0709@gmail.com>
2022-02-28 15:56:16 +0900

    -

--- Online_Judge/BaekJoon/0000-4999/Note.py
+++ Online_Judge/BaekJoon/0000-4999/Note.py
@@ -1,4 +1,19 @@
-f = open('input.txt', "r")
+import sys
+from itertools import combinations
 
-string = f.read
-print(string)
+N = int(sys.stdin.readline())
+
+ans = 0
+nums = list()
+
+for i in range(1, 11):
+    for comb in combinations(range(0,10), i):
+        comb = list(comb)
+        comb.sort(reverse=True)
+        nums.append(int("".join(map(str, comb))))
+
+nums.sort()
+try:
+    print(nums[N])
+except:
+    print(-1)
