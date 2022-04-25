fceb51b8e2b5e9be9bf4f4ea4f0d5674d9fdd093
leejw0709@gmail.com <leejw0709@gmail.com>
2022-04-25 20:14:20 +0900

    -

---
+++ Language/C/10_Constant/10_9_judge_literal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ 
+int main()
+{
+    //Hello, world! 76 -2147483647 4.528172
+    printf("%s %o %lld %Lf\n", "Hello, world!", 076, -2147483647LL, 4.528172L);
+ 
+    return 0;
+}
---
+++ Language/C/11_Variable/11_7_Input_Variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    float b;
+    char c;
+    
+    // 10 1.234567 a
+    scanf("%d %f %c", &a, &b, &c);
+    printf("%d\n%f\n%c", a, b, c);
+
+    return 0;
+}
