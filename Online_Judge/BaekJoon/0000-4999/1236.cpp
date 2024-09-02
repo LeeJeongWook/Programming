a10981b07232fbe6473fe229c9f4a664d0558f23
leejw0709@gmail.com <leejw0709@gmail.com>
2024-09-02 23:23:43 +0900

    -

--- Online_Judge/BaekJoon/0000-4999/1236.cpp
+++ Online_Judge/BaekJoon/0000-4999/1236.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main(){
-    
+    int N;
+    scanf("%d", &N);
+
+    int count = 0, x, y, r;
+
+    for(int i = 0; i < N; i++){
+        scanf("%d %d %d", &x, &y, &r);
+        if(r > 50){
+           count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
 }
